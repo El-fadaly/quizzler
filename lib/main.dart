ed9197dc628e1e9cec6fbae0dae12fc6a533fe31
El-fadaly <45339363+El-fadaly@users.noreply.github.com>
2021-05-17 05:12:17 +0200

    first commit adding files

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:quizzler/quizBrain.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
 
 void main() => runApp(Quizzler());
+
+QuizBrain quizBrain = QuizBrain();
 
 class Quizzler extends StatelessWidget {
   @override
@@ -25,6 +29,35 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> scroeKeeper = [];
+
+  void checkAnswer(bool userPickedAnswer) {
+    if (quizBrain.isFinished()) {
+      Alert(
+              context: context,
+              title: 'Hooray',
+              desc: 'You have finished the quiz')
+          .show();
+      quizBrain.reset();
+      scroeKeeper = [];
+    } else {
+      setState(() {
+        if (quizBrain.getAnswer() == userPickedAnswer) {
+          scroeKeeper.add(Icon(
+            Icons.done,
+            color: Colors.green,
+          ));
+        } else {
+          scroeKeeper.add(Icon(
+            Icons.close,
+            color: Colors.red,
+          ));
+        }
+        quizBrain.nextQuestion();
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +70,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                quizBrain.getQuestionText(),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -50,9 +83,8 @@ class _QuizPageState extends State<QuizPage> {
         Expanded(
           child: Padding(
             padding: EdgeInsets.all(15.0),
-            child: FlatButton(
-              textColor: Colors.white,
-              color: Colors.green,
+            child: TextButton(
+              style: TextButton.styleFrom(backgroundColor: Colors.green),
               child: Text(
                 'True',
                 style: TextStyle(
@@ -62,6 +94,7 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked true.
+                checkAnswer(true);
               },
             ),
           ),
@@ -69,8 +102,8 @@ class _QuizPageState extends State<QuizPage> {
         Expanded(
           child: Padding(
             padding: EdgeInsets.all(15.0),
-            child: FlatButton(
-              color: Colors.red,
+            child: TextButton(
+              style: TextButton.styleFrom(backgroundColor: Colors.red),
               child: Text(
                 'False',
                 style: TextStyle(
@@ -79,12 +112,13 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked false.
+                //The user picked true.
+                checkAnswer(true);
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(children: scroeKeeper)
       ],
     );
   }
